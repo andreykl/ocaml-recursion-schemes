8c9ada4822424640837e19d3d826a23827462e09
Andrey <deepmindster@gmail.com>
2025-12-25 15:27:31 +0300

    wip

--- plant.ml
+++ plant.ml
@@ -1,26 +1,23 @@
-module Plant = struct
-  module F = struct
-    type 'a t = Root of 'a | Stalk of 'a | Fork of 'a * 'a * 'a | Bloom
+module F = struct
+  type 'a t = Root of 'a | Stalk of 'a | Fork of 'a * 'a * 'a | Bloom
 
-    let map f = function
-      | Root a -> Root (f a)
-      | Stalk a -> Stalk (f a)
-      | Fork (l, c, r) -> Fork (f l, f c, f r)
-      | Bloom -> Bloom
-  end
-
-  include F
-  module R = Recursion.Schemes.Make (F)
+  let map f = function
+    | Root a -> Root (f a)
+    | Stalk a -> Stalk (f a)
+    | Fork (l, c, r) -> Fork (f l, f c, f r)
+    | Bloom -> Bloom
 end
+
+include F
+module R = Recursion.Schemes.Make (F)
 
 let grow seed : Action.t * Seed.t =
   let act = Action.of_int_in_range (Seed.int_in_range ~seed) in
   (act, Seed.inc seed)
 
-let sow : Seed.t Plant.R.cvcoalgebra =
+let sow : Seed.t R.cvcoalgebra =
  fun seed ->
   let act, next = grow seed in
-  let open Plant in
   if Seed.is_min seed then Root (R.Automatic next)
   else if Seed.is_max seed then Bloom
   else
