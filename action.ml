cb0aba1eabdefa68fe5fe59c83cb20d634957d06
Andrey <deepmindster@gmail.com>
2025-12-25 15:29:59 +0300

    wip

--- action.ml
+++ action.ml
@@ -1,5 +1,5 @@
 type t = Flower | Upwards | Branch
 
-let of_int_in_range (f : min:int -> max:int -> int) : t =
-  let v = f ~min:1 ~max:5 in
+let of_int_in_range (int_in_range : min:int -> max:int -> int) : t =
+  let v = int_in_range ~min:1 ~max:5 in
   if v < 2 then Flower else if v < 3 then Branch else Upwards
