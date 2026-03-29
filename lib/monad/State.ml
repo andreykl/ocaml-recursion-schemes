b5ad50d7f7998883504a4fc81d17c53eff2fb3e5
Andrey <aklaus.code@gmail.com>
2026-03-29 20:53:42 +0300

    wip

--- lib/monad/State.ml
+++ lib/monad/State.ml
@@ -1,12 +1,12 @@
 module type State = sig
   type state
 
-  include MonadBase.Monad (* with type 'a t = state -> state * 'a *)
+  include MonadBase.Monad (* with type 'a t = state -> 'a * state *)
 
   val get : state t
   val put : state -> unit t
   val modify : (state -> state) -> unit t
-  val runState : 'a t -> state -> state * 'a
+  val runState : 'a t -> state -> 'a * state
 end
 
 module Make (S : sig
@@ -14,12 +14,12 @@ module Make (S : sig
 end) : State with type state = S.state = struct
   module M = struct
     type state = S.state
-    type 'a t = state -> state * 'a
+    type 'a t = state -> 'a * state
 
-    let return v s = (s, v)
+    let return v s = (v, s)
 
     let bind m k s =
-      let s', v = m s in
+      let v, s' = m s in
       k v s'
   end
 
@@ -27,7 +27,7 @@ end) : State with type state = S.state = struct
   include MonadBase.Make (M)
 
   let get s = (s, s)
-  let modify f s = (f s, ())
-  let put s _ = (s, ())
+  let modify f s = ((), f s)
+  let put s _ = ((), s)
   let runState m init = m init
 end
--- lib/recursion_schemes_ppx/recursion_schemes_ppx.ml
+++ lib/recursion_schemes_ppx/recursion_schemes_ppx.ml
@@ -30,46 +30,88 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
   let open Ast_builder.Make (struct
     let loc = loc
   end) in
+  let module State = Monad.State.Make (struct
+    type state = StringSet.t
+  end) in
+  let open State in
+  let open State.Syntax in
+  let fresh : string State.t =
+    let* s = get in
+    let v, s = fresh_name letters s in
+    let* () = put s in
+    return v
+  in
+
+  let collect_names_in_type typ =
+    let rec collect acc = function
+      | { ptyp_desc = Ptyp_var name; _ } -> StringSet.add name acc
+      | { ptyp_desc = Ptyp_constr (_, args); _ } ->
+          List.fold_left collect acc args
+      | { ptyp_desc = Ptyp_tuple types; _ } -> List.fold_left collect acc types
+      | { ptyp_desc = Ptyp_arrow (_, t1, t2); _ } -> collect (collect acc t1) t2
+      | { ptyp_desc = Ptyp_poly (vars, typ); _ } ->
+          let acc =
+            List.fold_left (fun acc var -> StringSet.add var.txt acc) acc vars
+          in
+          collect acc typ
+      | _ -> acc
+    in
+    collect StringSet.empty typ
+  in
+
   let build_case ({ pcd_name; pcd_args; _ } : constructor_declaration)
       ~(transform_var : string -> expression -> expression) ~map_pat_ident
       ~map_exp_ident : case =
-    let args_to_pat_expr (args : constructor_arguments) :
-        pattern option * expression option =
-      match args with
+    let rec process_type : core_type -> (pattern * expression) State.t =
+      function
+      | { ptyp_desc = Ptyp_var lbl; _ } ->
+          let pat = ppat_var @@ Located.mk lbl in
+          let ident = pexp_ident @@ Located.lident lbl in
+          let arg = transform_var lbl ident in
+          return (pat, arg)
+      | { ptyp_desc = Ptyp_tuple typs; _ } ->
+          let* pes = mapM process_type typs in
+          let patterns, expressions = List.split pes in
+          return (ppat_tuple patterns, pexp_tuple expressions)
+      | { ptyp_desc = Ptyp_constr (_, []); _ } ->
+          (* concrete type like int, string -- no type parameters,
+                  just pass through *)
+          let* name = fresh in
+          let pat = ppat_var @@ Located.mk name in
+          let expr = pexp_ident @@ Located.lident name in
+          return (pat, expr)
+      | { ptyp_desc = Ptyp_constr (lidnt, args); _ } ->
+          (* TODO: parametrized types inside constructors (e.g. 'a list)
+                  are not supported. General solution: require user to provide
+                  a map function via attribute, e.g.
+                  | Foo of (int list [@map List.map]) *)
+          let* pes = mapM process_type args in
+          let patterns, expressions = List.split pes in
+          ( ppat_construct lidnt @@ ppat_tuple_opt patterns,
+            pexp_construct lidnt @@ pexp_tuple_opt expressions )
+          |> return
+      | _ ->
+          (* TODO: handle other types (e.g. Ptyp_arrow, Ptyp_alias);
+                  for now generate a fresh variable and pass through *)
+          let* name = fresh in
+          return (ppat_var @@ Located.mk name, pexp_ident @@ Located.lident name)
+    in
+    let pat_idnt = Located.map_lident @@ map_pat_ident pcd_name in
+    let exp_idnt = Located.map_lident @@ map_exp_ident pcd_name in
+    let pato, expro =
+      match pcd_args with
       | Pcstr_tuple args ->
-          let rec map_core_type_pe : core_type -> pattern * expression =
-            function
-            | { ptyp_desc = Ptyp_var lbl; _ } ->
-                let pat = ppat_var @@ Located.mk lbl in
-                let ident = pexp_ident (Located.lident lbl) in
-                let arg = transform_var lbl ident in
-                (pat, arg)
-            | { ptyp_desc = Ptyp_tuple typs; _ } ->
-                let patterns, expressions =
-                  List.split @@ List.map map_core_type_pe typs
-                in
-                (ppat_tuple patterns, pexp_tuple expressions)
-            | { ptyp_desc = Ptyp_constr (lidnt, args); _ } ->
-                let patterns, expressions =
-                  List.split @@ List.map map_core_type_pe args
-                in
-                ( ppat_construct lidnt @@ ppat_tuple_opt patterns,
-                  pexp_construct lidnt @@ pexp_tuple_opt expressions )
-            | _ ->
-                (* TODO: support other types correctly? *)
-                (ppat_var @@ Located.mk "x", pexp_ident @@ Located.lident "x")
-          in
           let patterns, expressions =
-            List.split @@ List.map map_core_type_pe args
+            let pes, _ =
+              State.runState (State.mapM process_type args) StringSet.empty
+            in
+            List.split pes
           in
           (ppat_tuple_opt patterns, pexp_tuple_opt expressions)
       | Pcstr_record _labels ->
           (* TODO: support records *)
           (None, None)
     in
-    let pat_idnt = Located.map_lident @@ map_pat_ident pcd_name in
-    let exp_idnt = Located.map_lident @@ map_exp_ident pcd_name in
-    let pato, expro = args_to_pat_expr pcd_args in
     case ~guard:None
       ~lhs:(ppat_construct pat_idnt pato)
       ~rhs:(pexp_construct exp_idnt expro)
@@ -86,39 +128,25 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
        ptype_params = params;
        _;
      };
-      ] ->
-       let projFunName, embFunName = "project", "embed" in
-       let rsModName, projModName, embModName, baseModName =
-         if tname = "t" then
-           tname, "Project", "Embed", "Base"
-         else
-           let ctname = String.capitalize_ascii tname in
-           "RS" ^ctname, "Project" ^ ctname, "Embed" ^ ctname, "Base" ^ ctname
-       in
-       let incl = 
-         if tname = "t" then
-           pstr_include {
-               pincl_mod = pmod_apply
-                             (pmod_apply
-                                (pmod_apply
-                                   (pmod_ident @@ Located.lident "Recursion_schemes.Make")
-                                   (pmod_ident @@ Located.lident baseModName))
-                                (pmod_ident @@ Located.lident projModName))
-                             (pmod_ident @@ Located.lident embModName);
-               pincl_loc = loc;
-               pincl_attributes = [];
-             }
-         else
-           pstr_module (module_binding
-                          ~name:(Located.mk (Some rsModName))
-                          ~expr:(pmod_apply
-                                   (pmod_apply
-                                      (pmod_apply
-                                         (pmod_ident @@ Located.lident "Recursion_schemes.Make")
-                                         (pmod_ident @@ Located.lident baseModName))
-                                      (pmod_ident @@ Located.lident projModName))
-                                   (pmod_ident @@ Located.lident embModName)))
-        in               
+    ] ->
+        let ( proj_fun,
+              emb_fun,
+              proj_mod,
+              emb_mod,
+              base_mod,
+              std_typ_name,
+              rs_mod,
+              elem_sig ) =
+          ("project", "embed", "Project", "Embed", "Base", "t", "RS", "Elem")
+        in
+        let rs_mod =
+          if tname = std_typ_name then rs_mod
+          else rs_mod ^ String.capitalize_ascii tname
+        in
+        let make_rs_mod = "Make_" ^ rs_mod in
+        let incl =
+          [%str include Recursion_schemes.Make (Base) (Project) (Embed)]
+        in
 
         let param_names =
           List.fold_left
@@ -127,27 +155,6 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
               | { ptyp_desc = Ptyp_var name; _ } -> StringSet.add name acc
               | _ -> acc)
             StringSet.empty params
-        in
-
-        let collect_names_in_type typ =
-          let rec collect acc = function
-            | { ptyp_desc = Ptyp_var name; _ } -> StringSet.add name acc
-            | { ptyp_desc = Ptyp_constr (_, args); _ } ->
-                List.fold_left collect acc args
-            | { ptyp_desc = Ptyp_tuple types; _ } ->
-                List.fold_left collect acc types
-            | { ptyp_desc = Ptyp_arrow (_, t1, t2); _ } ->
-                collect (collect acc t1) t2
-            | { ptyp_desc = Ptyp_poly (vars, typ); _ } ->
-                let acc =
-                  List.fold_left
-                    (fun acc var -> StringSet.add var.txt acc)
-                    acc vars
-                in
-                collect acc typ
-            | _ -> acc
-          in
-          collect StringSet.empty typ
         in
 
         let names_in_constructors =
@@ -177,9 +184,10 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
             let ct = { ct with ptyp_loc = loc } in
             match ct.ptyp_desc with
             | Ptyp_constr ({ txt = Lident tname'; _ }, _) ->
-                if tname = tname' then
-                  { ct with ptyp_desc = Ptyp_var param_name }
-                else ct
+                if param_name = tname' then
+                  ptyp_var tname
+                else
+                  { ct with ptyp_desc = Ptyp_constr ({ txt = Ldot (Lident elem_sig, tname'); loc }, []) }
             | _ -> ct
           in
           let args =
@@ -213,11 +221,11 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
           }
         in
         let params =
-          (ptyp_var param_name, (NoVariance, NoInjectivity)) :: params
+          [ptyp_var param_name, (NoVariance, NoInjectivity)] (* :: params *)
         in
         let ctors = List.map map_constructor ctors in
         (* In Project, constructor labels are qualified in expressions (Base.Ctor);
-           in Embed — in patterns. So, map_pat_ident and map_exp_ident are swapped
+           in Embed -- in patterns. So, map_pat_ident and map_exp_ident are swapped
            between Project and Embed. *)
         let make_projection_modules ~map_pat_ident ~map_exp_ident ~module_name
             ~fun_name =
@@ -240,28 +248,29 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
             pmod_structure
               [
                 pstr_module
-                  (module_binding ~name:(Located.mk (Some "Base"))
-                     ~expr:(pmod_ident @@ Located.lident baseModName));
+                  (module_binding
+                     ~name:(Located.mk @@ Some base_mod)
+                     ~expr:(pmod_ident @@ Located.lident base_mod));
                 pstr_type Nonrecursive [ t_decl ];
                 pstr_value Nonrecursive
                   [
                     value_binding
-                      ~pat:(ppat_var (Located.mk fun_name))
+                      ~pat:(ppat_var @@ Located.mk fun_name)
                       ~expr:the_fun;
                   ];
               ]
           in
           pstr_module
-            (module_binding ~name:(Located.mk (Some module_name)) ~expr:body)
+            (module_binding ~name:(Located.mk @@ Some module_name) ~expr:body)
         in
         let base_module =
           let base_functor_decl : type_declaration =
-            type_declaration ~name:(Located.mk tname) ~params ~cstrs:[]
+            type_declaration ~name:(Located.mk "t") ~params ~cstrs:[]
               ~kind:(Ptype_variant ctors) ~private_:Public ~manifest:None
           in
           let transform_var lbl ident =
             if lbl = param_name then
-              pexp_apply (pexp_ident (Located.lident "f")) [ (Nolabel, ident) ]
+              pexp_apply (pexp_ident @@ Located.lident "f") [ (Nolabel, ident) ]
             else ident
           in
           let cases =
@@ -282,19 +291,56 @@ let str_type_decl ~ctxt (rec_flag, tdecls) =
 
               let map = [%e map_function]
             end]
+          (*          let body =
+            pmod_structure
+              [
+                pstr_type Nonrecursive [ base_functor_decl ];
+                pstr_value Nonrecursive
+                  [
+                    value_binding
+                      ~pat:(ppat_var @@ Located.mk "map")
+                      ~expr:map_function;
+                  ];
+              ]
+          in
+          pstr_module
+            (module_binding ~name:(Located.mk @@ Some base_mod) ~expr:body)
+ *)
         in
         let project_module =
           make_projection_modules ~map_pat_ident:Fun.id
-            ~map_exp_ident:(Located.map (fun l -> baseModName ^ "." ^ l))
-            ~module_name:projModName ~fun_name:projFunName
+            ~map_exp_ident:(Located.map (fun l -> base_mod ^ "." ^ l))
+            ~module_name:proj_mod ~fun_name:proj_fun
         in
 
         let embed_module =
           make_projection_modules ~map_exp_ident:Fun.id
-            ~map_pat_ident:(Located.map (fun l -> baseModName ^ "." ^ l))
-            ~module_name:embModName ~fun_name:embFunName
+            ~map_pat_ident:(Located.map (fun l -> base_mod ^ "." ^ l))
+            ~module_name:emb_mod ~fun_name:emb_fun
         in
-        base_module @ [ project_module; embed_module ; incl ]
+        let elem_type_decls =
+          List.map
+            (fun name ->
+              type_declaration ~name:(Located.mk name) ~params:[] ~cstrs:[]
+                ~private_:Public ~manifest:None ~kind:Ptype_abstract)
+            (StringSet.to_list param_names)
+        in
+
+        let module_make_rs =
+          pstr_module
+            (module_binding
+               ~name:(Located.mk @@ Some make_rs_mod)
+               ~expr:
+                 (pmod_functor
+                    (Named
+                       ( Located.mk @@ Some elem_sig,
+                         pmty_signature
+                           [ psig_type Nonrecursive elem_type_decls ] ))
+                    (pmod_structure @@ base_module
+                    @ [ project_module; embed_module ]
+                    @ incl)))
+        in
+        [ module_make_rs ]
     | _ ->
         Location.raise_errorf ~loc
           "recursion_schemes can be derived for variant types only"
